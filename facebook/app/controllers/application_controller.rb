47b31c00b2a935f7e6b30f410746c7b6111bcc6f
jleung411 <jleung411@gmail.com>
2015-08-07 17:36:58 +0000

    basic functionality working

--- facebook/app/controllers/application_controller.rb
+++ facebook/app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :parse_facebook_cookies
   def parse_facebook_cookies
-    logger.info @facebook_cookies
-    logger.info "parse_facebook_cookies"
-#   @facebook_cookies ||= Koala::Facebook::OAuth.new("411274062413360", "0d8d17b1f0c10af1f0481644c5ad0cdf").get_user_info_from_cookie(cookies)
-    logger.info @facebook_cookies
+#    @facebook_cookies ||= Koala::Facebook::OAuth.new("412298645644235", "58b2e772c8e57d3baea107ea13b48c5e").get_user_info_from_cookie(cookies)
   end
 
 end
--- facebook/app/controllers/posts_controller.rb
+++ facebook/app/controllers/posts_controller.rb
@@ -5,46 +5,60 @@ require "fbgraph"
 # https://github.com/arsduo/koala/wiki/Batch-requests
 # https://github.com/arsduo/koala/wiki/Acting-as-a-Page
 
+# notes:
+# posting to pages as user
+# posting to page as a page required FB to approve app
+
+
 class PostsController < ApplicationController
 
   def new
-    @post = Post.new
-
-    logger.info @facebook_cookies
-    @user_graph = Koala::Facebook::API.new(@facebook_cookies['access_token'])
-    pages = @user_graph.get_connections('me', 'accounts')
-
-    for page in pages
-
-      if page['name']
-        next
-      end 
-
-      logger.info page
-      logger.info page['access_token']
-      @page_graph = Koala::Facebook::API.new(page['access_token']) 
-      profile = @page_graph.get_object('me')
-      logger.info profile
-
-      @page_graph.put_wall_post('Hello there!', {
-               'name' => 'Link name',
-               'link' => 'http://www.example.com/',
-               'caption' => '{*actor*} posted a new review',
-               'description' => 'This is a longer description of the attachment',
-               'picture' => 'http://www.example.com/thumbnail.jpg'
-             })
+    begin
+      @post = Post.new
+    rescue
+      logger.info 'exception'
     end
   end
 
   def create
     begin
-       @post = Post.new(params.require(:post).permit(:title, :text))
-       @post.save
-       redirect_to @post
-    rescue
+      @post = Post.new(params.require(:post).permit(:title, :description, :link, :message))
+
+#      if @post.save!
+#         @user_graph= Koala::Facebook::API.new(@facebook_cookies['access_token'])
+         @user_graph= Koala::Facebook::API.new('CAAF2ZB8JQo8sBANxbGLCZBrZB667HhhZCJ08ptWknVeHHfq2PEfI1GLghcwZAdoZAr2afeJ22RacqwKYQCBuQNhEnC4ZBrP98k6nNDbcHgFF8dL0epwV09TZBrk4KCl0xGQytVZCSLvjX5K0iDB7sZCNdtYZC2FojQg2mWHO2FDg5pUi6FyagHdtEvdrbS4cheAH2dFDngwKvhXpOfBbpuNSP4LnPr5G0GKUggZD')
+         @pages = @user_graph.get_connections('me', 'accounts')
+
+         post_ids = []
+
+         for page in @pages
+           @page_graph = Koala::Facebook::API.new(page['access_token'])
+           result = @page_graph.batch do |batch_api|
+              batch_api.put_connections('me', 'feed',
+                                        :message => @post.message,
+                                        :caption => @post.title,
+                                        :description => @post.description,
+                                        :name => @post.title,
+                                        :picture => '',
+                                        :link => @post.link)
+           end
+           post_ids.push({:post_id => result[0]['id'], :page_name => page['name']})
+         end
+
+      logger.info @post['id']
+      @post.save!
+      redirect_to :controller => 'posts', :action => 'show', :id => 13, :post_ids => post_ids
+    rescue => e
       logger.info 'exception'
+      logger.info e
+      logger.info e.message
+#      if(e.fb_error_type == 'OAuthException')
+        # Already Posted
+#      end
+      render new
     end
   end
+
 
   def show
     @post = Post.find(params[:id])
